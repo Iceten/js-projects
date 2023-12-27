84c9d38a59dc8fabddd7cb2e45ab4ca80d044c26
idriss <idrissmouliom@gmail.com>
2023-12-27 03:04:25 +0100

    Password Generator

---
+++ password-generator/app.js
@@ -0,0 +1,76 @@
+const passwordShow = document.querySelector("#password-display");
+const inputCharRange = document.querySelector("#range-char");
+const inputCharNum = document.querySelector("#number-char");
+
+const numbers = document.querySelector("#numbers");
+const symbols = document.querySelector("#symbols");
+const uppercase = document.querySelector("#uppercase");
+const submit = document.querySelector("input[type=submit]");
+
+const lowerCaseCharCodes = arrayLowtoHigh(97, 122);
+const higherCaseCharCodes = arrayLowtoHigh(65, 90);
+const numberCharCodes = arrayLowtoHigh(48, 57);
+const symbolCharCodes = arrayLowtoHigh(33, 47)
+  .concat(arrayLowtoHigh(58, 64))
+  .concat(arrayLowtoHigh(91, 96))
+  .concat(arrayLowtoHigh(123, 126));
+
+inputCharRange.addEventListener("input", () => {
+  inputCharNum.value = inputCharRange.value;
+});
+inputCharNum.addEventListener("input", () => {
+  inputCharRange.value = inputCharNum.value;
+});
+
+submit.addEventListener("click", (e) => {
+  e.preventDefault();
+  const password = generatePassword(
+    inputCharNum.value,
+    numbers.checked,
+    symbols.checked,
+    uppercase.checked
+  );
+  passwordShow.innerText = password;
+});
+
+function generatePassword(
+  characterNumber,
+  hasNumbers,
+  hasSymbols,
+  isUppercase
+) {
+  let charCodes;
+
+  if (isUppercase) {
+    charCodes = higherCaseCharCodes;
+  } else {
+    charCodes = lowerCaseCharCodes;
+  }
+
+  if (hasNumbers) charCodes = charCodes.concat(numberCharCodes);
+
+  if (hasSymbols) charCodes = charCodes.concat(symbolCharCodes);
+
+  let characterCodes = [];
+  const password = [];
+
+  for (let i = 0; i < characterNumber; i++) {
+    characterCodes.push(
+      charCodes[Math.floor(Math.random() * charCodes.length)]
+    );
+    password.push(String.fromCharCode(characterCodes[i]));
+  }
+
+  console.log(charCodes);
+  console.log("higher " + higherCaseCharCodes);
+  //   console.log(password.join(""));
+  return password.join("");
+}
+
+function arrayLowtoHigh(low, high) {
+  let array = [];
+  for (let i = low; i <= high; i++) {
+    array.push(i);
+  }
+  return array;
+}
